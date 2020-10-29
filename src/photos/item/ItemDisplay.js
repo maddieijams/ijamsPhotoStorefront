7352e2ef7b9943535a5f4c4980265b409f05d74e
Maddie Ijams <maddieijams@gmail.com>
2020-10-29 07:31:45 -0400

    update styling for photo detail

--- src/photos/item/ItemDisplay.js
+++ src/photos/item/ItemDisplay.js
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
-import { FormGroup, Label, Input, Button, Container, Row, Col } from "reactstrap";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 import { Link } from "react-router-dom";
-import APIURL from '../../helpers/environment';
+import APIURL from "../../helpers/environment";
 
 class ItemDisplay extends Component {
   constructor(props) {
@@ -9,7 +17,7 @@ class ItemDisplay extends Component {
     this.state = {
       photo: "",
       id: "",
-      price: 30
+      price: 30,
     };
   }
 
@@ -23,79 +31,83 @@ class ItemDisplay extends Component {
     fetch(`${APIURL}/photo/item/${this.id}`, {
       method: "GET",
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
-      .then(res => res.json())
-      .then(item =>
-        this.setState({ photo: item }, () => console.log(this.state.photo))
-      );
+      .then((res) => res.json())
+      .then((item) => this.setState({ photo: item }));
   };
 
   framePrice = () => {
-    this.setState({ price: 45 }, () => console.log(this.state.price));
+    this.setState({ price: 45 });
   };
 
   printPrice = () => {
-    this.setState({ price: 30 }, () => console.log(this.state.price));
+    this.setState({ price: 30 });
   };
 
   render() {
     return (
-      <div className='itemContainer'>
+      <div className="itemContainer">
         {this.state.photo ? (
           <div>
-
             <Container>
-              <Row >
-                <Col className='text-center'>
-            <h1>{this.state.photo.title}</h1>
-            <img
-              src={this.state.photo.imageURL}
-              alt="item"
-              width="300px"
-              height="auto"
-            />
-            <br />
-            <Link to={`/image/${this.id}`}>
-              <Button>View high resolution image</Button>
-            </Link>
-            </Col>
-            <Col className='text-center'>
-            <FormGroup tag="fieldset">
-            
-              <legend>How would you like it?</legend>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="frame" onClick={this.framePrice} />{" "}
-                  In a cool frame!
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="frame" onClick={this.printPrice} />{" "}
-                  No frame, just the print.
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="size" /> 8x10in
-                </Label>
-              </FormGroup>
-              <FormGroup check>
-                <Label check>
-                  <Input type="radio" name="size" /> 5x7in
-                </Label>
-              </FormGroup>
-            </FormGroup>
-            <h4>Price: ${this.state.price}</h4>
-            <Link to="/address">
-              <Button>Continue Checkout</Button>
-            </Link>
-            </Col>
-            </Row>
-            </Container>
+              <Row>
+                <Col className="text-center">
+                  <h3>{this.state.photo.title}</h3>
+                  <img
+                    src={this.state.photo.imageURL}
+                    alt="item"
+                    width="300px"
+                    height="auto"
+                  />
 
+                  <Link to={`/image/${this.id}`}>
+                    <Button className="mt-3">View high resolution image</Button>
+                  </Link>
+                </Col>
+                <Col className="text-center">
+                  <FormGroup tag="fieldset">
+                    <b>Frame options:</b>
+                    <FormGroup check>
+                      <Label check>
+                        <Input
+                          type="radio"
+                          name="frame"
+                          onClick={this.framePrice}
+                        />{" "}
+                        In a cool frame!
+                      </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                      <Label check>
+                        <Input
+                          type="radio"
+                          name="frame"
+                          onClick={this.printPrice}
+                        />{" "}
+                        No frame, just the print.
+                      </Label>
+                    </FormGroup>
+                    <b className="mt-3">Size options:</b>
+                    <FormGroup check>
+                      <Label check>
+                        <Input type="radio" name="size" /> 8x10in
+                      </Label>
+                    </FormGroup>
+                    <FormGroup check>
+                      <Label check>
+                        <Input type="radio" name="size" /> 5x7in
+                      </Label>
+                    </FormGroup>
+                  </FormGroup>
+                  <b>Price: ${this.state.price}</b>
+                  <Link to="/address">
+                    <Button>Continue Checkout</Button>
+                  </Link>
+                </Col>
+              </Row>
+            </Container>
           </div>
         ) : (
           <div />
